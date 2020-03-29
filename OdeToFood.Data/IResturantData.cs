2a862860bc477653ca46bde08e7141b9687826e9
Nathan <nathan.ballantyne@gmail.com>
2020-03-29 17:29:41 +1300

    Initial Git push

---
+++ OdeToFood.Data/IResturantData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IResturantData
+    {
+        IEnumerable<Restaurent> GetAll();
+    }
+
+    public class InMemoryResturantData : IResturantData
+    {
+        readonly List<Restaurent> restaurents;
+        public InMemoryResturantData()
+        {
+            restaurents = new List<Restaurent>()
+            {
+                new Restaurent{Id=1, Location="Whakatane", Cuisine=CuisineType.Italian, Name="Nathans Pizza"},
+                new Restaurent{Id=2, Location="Auckland", Cuisine=CuisineType.Mexican, Name="Carlos Spicy Mexican"},
+                new Restaurent{Id=3, Location="Tauranga", Cuisine=CuisineType.Indian, Name="Gados Indian Restuarent"}
+            };
+        }
+        public IEnumerable<Restaurent> GetAll()
+        {
+            return from r in restaurents
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        public string Message { get; set; }
+        public IResturantData resturantData { get; }
+        public ListModel(IConfiguration config, IResturantData resturantData)
+        {
+            this.config = config;
+            this.resturantData = resturantData;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"];
+        }
+
+        
+    }
+}
